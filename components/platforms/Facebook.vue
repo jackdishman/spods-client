c37c0fe6eb55e7a619d2c7c82b60739fdfcd813f
jackdishman <jdish123@gmail.com>
2020-08-18 00:45:37 -0400

    fixing error of missing facebook

---
+++ components/platforms/Facebook.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="bg-white">
+    <h2>{{ this.$props.verified.name }}</h2>
+    <img :src="this.$props.verified.picture.data.url" :width="this.$props.verified.picture.data.width" :height="this.$props.verified.picture.data.height" />
+    <h3>Friends: {{ this.$props.verified.friends.summary.total_count }}</h3>
+    <a class="text-green-500 text-xl" :href="this.$props.verified.link" target="_blank" rel="noreferrer">Visit Profile</a>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    verified: Object
+  }
+}
+</script>
---
+++ components/platforms/Snapchat.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="bg-white">
+    <h2>{{ this.$props.verified.me.displayName }}</h2>
+    <img :src="this.$props.verified.me.bitmoji.avatar" alt="">
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    verified: Object
+  }
+}
+</script>
